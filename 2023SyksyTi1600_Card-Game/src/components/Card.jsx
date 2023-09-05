0b71bcedee2c5e26019c8e6f9cbec0b50a2b15fd
Your Name <you@example.com>
2023-09-05 16:59:06 +0300

    card component luotiin

---
+++ 2023SyksyTi1600_Card-Game/src/components/Card.jsx
@@ -0,0 +1,9 @@
+
+export default function Card(){
+    return(
+        <div>
+            <span>Kissakortti</span>
+            <img src = "http://placekitten.com/120/100"/>
+        </div>
+    );
+}
