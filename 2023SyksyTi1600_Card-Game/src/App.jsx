921d02ba7fef8219c1f1039211d02508a15cf2f0
Your Name <you@example.com>
2023-09-19 16:56:55 +0300

    voittaja tsekkaus kesken

--- 2023SyksyTi1600_Card-Game/src/App.jsx
+++ 2023SyksyTi1600_Card-Game/src/App.jsx
@@ -8,11 +8,28 @@ const playerCard = {
           {name:'Weight', value: 100}],
 };
 
+const opponentCard = {
+  image: 'http://placekitten.com/120/100?image=2',
+  stats: [{name:'Cuteness', value: 99},
+          {name:'Speed', value: 123},
+          {name:'Weight', value: 44}],
+};
+
 export default function App(){
+  
+  function compareCards(){
+    console.log("Button clicked!")
+    const playerStat = playerCard.stats[0];
+    const opponentStat = opponentCard.stats[0];
+    
+  }
+
   return(
     <>
       <h1>Hello World!</h1>
       <Card card={playerCard} />
+      <button onClick={compareCards} type="button">Play</button>
+      <Card card={opponentCard} />
     </>
   );
 }
